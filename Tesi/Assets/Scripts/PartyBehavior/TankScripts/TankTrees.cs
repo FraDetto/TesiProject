d2e7c7d9727fe0e515d2c2728e070479f0f2c290
FraDetto <francesco030996@gmail.com>
2021-03-15 17:57:35 +0100

    Created the structure for Decision Tree for Attack State of Tank Behavior

--- Tesi/Assets/Scripts/PartyBehavior/TankScripts/TankTrees.cs
+++ Tesi/Assets/Scripts/PartyBehavior/TankScripts/TankTrees.cs
@@ -9,29 +9,34 @@ public class TankTrees : MonoBehaviour
     private Rigidbody myRigidbody;
     private DecisionTree dt;
 
-    void AttackTransitionTree()
+    private void AttackTransitionTree()
     {
         // Define actions
         DTAction a1 = new DTAction(chaseBoos);
         DTAction a2 = new DTAction(attackTheBoss);
         DTAction a3 = new DTAction(defendFromTheBoss);
         DTAction a4 = new DTAction(chooseAttackOrDef);
+        DTAction a5 = new DTAction(useSpecial);
 
         // Define decisions
-        DTDecision d1 = new DTDecision(checkBossPosition);
-        DTDecision d2 = new DTDecision(checkConditionForDef);
-        DTDecision d3 = new DTDecision(checkBossUlting);
+        DTDecision d1 = new DTDecision(checkConditionsForSpecial);
+        DTDecision d2 = new DTDecision(checkBossPosition);
+        DTDecision d3 = new DTDecision(checkConditionForNotDef);
+        DTDecision d4 = new DTDecision(checkBossUlting);//controllare se scudo up
 
 
         // Link action with decisions
         d1.AddLink(false, d2);
-        d1.AddLink(true, a1);
+        d1.AddLink(true, a5);
 
-        d2.AddLink(true, a2);
+        d2.AddLink(true, a1);
         d2.AddLink(false, d3);
 
-        d3.AddLink(true, a3);
-        d3.AddLink(false, a4);
+        d3.AddLink(true, a2);
+        d3.AddLink(false, d4);
+
+        d4.AddLink(true, a3);
+        d4.AddLink(false, a4);
 
         // Setup my DecisionTree at the root node
         dt = new DecisionTree(d1);
@@ -39,10 +44,27 @@ public class TankTrees : MonoBehaviour
 
     }
 
-    public DecisionTree getAttackTransitionTree()
+    //CREATE AND GET METHODS
+    public DecisionTree CreateAndGetAttackTransitionTree()
     {
         AttackTransitionTree();
         return dt;
+    }
+
+    public DecisionTree getCreateAndGetAttackTransitionTree()
+    {
+        return dt;
+    }
+
+
+
+    public object checkConditionsForSpecial(object o)//is time to use the special?
+    {
+        if (reactionTime <= 0)
+        {
+            return false;
+        }
+        return true;
     }
 
     public object checkBossPosition(object o)//control if the boss is in range
@@ -54,7 +76,7 @@ public class TankTrees : MonoBehaviour
         return true;
     }
 
-    public object checkConditionForDef(object o)//see if is time to attack or to def
+    public object checkConditionForNotDef(object o)//see if is time to attack or to def
     {
         if (reactionTime <= 0)
         {
@@ -63,7 +85,7 @@ public class TankTrees : MonoBehaviour
         return true;
     }
 
-    public object checkBossUlting(object o)//is time to use the special?
+    public object checkBossUlting(object o)//is the boss ulting? is the shield up in case?
     {
         if (reactionTime <= 0)
         {
@@ -91,6 +113,12 @@ public class TankTrees : MonoBehaviour
     }
 
     public object chooseAttackOrDef(object o)
+    {
+
+        return null;
+    }
+
+    public object useSpecial(object o)
     {
 
         return null;
