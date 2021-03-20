d9719d3f99db6e74563fa38b2d503fdf252f19a8
FraDetto <francesco030996@gmail.com>
2021-03-20 13:04:55 +0100

    Added a scriptableObject to keep characters' data. The data put in the object are placeholders

---
+++ Tesi/Assets/Scripts/DataManagment/PartyData.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartyData : ScriptableObject
+{
+    /// <summary>
+    /// TANK VALUES
+    /// </summary>
+    ///
+
+    public float hpTank = 300.0f;
+    public float damageTank = 40.0f;
+    public float armorTank = 15.0f;
+
+    /// <summary>
+    /// BRUISER VALUES
+    /// </summary>
+    /// 
+    public float hpBruiser = 220.0f;
+    public float damageBruiser = 60.0f;
+    public float armorBruiser = 11.0f;
+
+    /// <summary>
+    /// MAGE VALUES
+    /// </summary>
+    /// 
+    public float hpMage = 150.0f;
+    public float damageMage = 90.0f;
+    public float armorMage = 9.0f;
+
+    /// <summary>
+    /// HEALER VALUES
+    /// </summary>
+    /// 
+    public float hpHealer = 180.0f;
+    public float damageHealer = 25.0f;
+    public float armorHealer = 9.0f;
+
+    /// <summary>
+    /// BOSS VALUES
+    /// </summary>
+    /// 
+    public float hpBoss = 300.0f;
+    public float damageBoss = 40.0f;
+    public float armorBoss = 15.0f;
+
+   
+}
