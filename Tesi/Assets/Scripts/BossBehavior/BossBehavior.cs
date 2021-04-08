76dfc26088f1e93366f1841a6fdf78597eb36ba0
FraDetto <francesco030996@gmail.com>
2021-04-08 15:43:42 +0200

    Start to work on agent Boss

--- Tesi/Assets/Scripts/BossBehavior/BossBehavior.cs
+++ Tesi/Assets/Scripts/BossBehavior/BossBehavior.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.MLAgents;
 
-public class BossBehavior : MonoBehaviour
+public class BossBehavior : Agent
 {
     // Start is called before the first frame update
     void Start()
