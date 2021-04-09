e411af8c11e6260c8d563eb2b618c0d76ac90c64
FraDetto <francesco030996@gmail.com>
2021-04-09 18:11:01 +0200

    Added empty methods, one for each attack of boss and relative coroutine for cooldown

--- Tesi/Assets/Scripts/BossBehavior/BossProfile.cs
+++ Tesi/Assets/Scripts/BossBehavior/BossProfile.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class BossProfile : moreSpecificProfile
 {
- 
-    public Collider collider;
    
     public string target;
     public bool isAttacking = false;
@@ -21,7 +19,7 @@ public class BossProfile : moreSpecificProfile
     private bool cooldownAoEAttk = false;
     private bool cooldownSwingAttk = false;
     private bool cooldownAheadAttk = false;
-    private bool cooldownbreakAttk = false;
+    private bool cooldownBreakAttk = false;
 
     // Start is called before the first frame update
 
@@ -38,6 +36,80 @@ public class BossProfile : moreSpecificProfile
     {
         publicSetLifeAfterDamage(damageFromCharacter);
         Debug.Log("OH NO SPELL MI HAI COLPITO " + publicGetCurrentLife());
+    }
+
+
+
+    public void rangedAttack()
+    {
+        cooldownRangedAttk = true;
+        StartCoroutine(startCooldownRangedAttk());
+    }
+
+    public IEnumerator startCooldownRangedAttk()
+    {
+        yield return new WaitForSeconds(1.4f);
+        cooldownRangedAttk = false;
+    }
+
+    public void jumpAttack()
+    {
+        cooldownJumpAttk = true;
+        StartCoroutine(startCooldownJumpAttk());
+    }
+
+    public IEnumerator startCooldownJumpAttk()
+    {
+        yield return new WaitForSeconds(1.4f);
+        cooldownJumpAttk = false;
+    }
+
+    public void AoEAttack()
+    {
+        cooldownAoEAttk = true;
+        StartCoroutine(startCooldownAoEAttk());
+    }
+
+    public IEnumerator startCooldownAoEAttk()
+    {
+        yield return new WaitForSeconds(1.4f);
+        cooldownAoEAttk = false;
+    }
+
+    public void SwingAttack()
+    {
+        cooldownSwingAttk = true;
+        StartCoroutine(startCooldownSwingAttk());
+    }
+
+    public IEnumerator startCooldownSwingAttk()
+    {
+        yield return new WaitForSeconds(1.4f);
+        cooldownSwingAttk = false;
+    }
+
+    public void aheadAttack()
+    {
+        cooldownAheadAttk = true;
+        StartCoroutine(startCooldownAheadAttk());
+    }
+
+    public IEnumerator startCooldownAheadAttk()
+    {
+        yield return new WaitForSeconds(1.4f);
+        cooldownAheadAttk = false;
+    }
+
+    public void breakAttack()
+    {
+        cooldownBreakAttk = true;
+        StartCoroutine(startCooldownBreakAttk());
+    }
+
+    public IEnumerator startCooldownBreakAttk()
+    {
+        yield return new WaitForSeconds(1.4f);
+        cooldownBreakAttk = false;
     }
 }
 
