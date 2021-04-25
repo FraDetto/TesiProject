d44c519b9fdf658f3739b8141e029a101c26b165
FraDetto <francesco030996@gmail.com>
2021-04-25 18:13:49 +0200

    Implemented correctly the method to spawn a random party

--- Tesi/Assets/Scripts/GameManager.cs
+++ Tesi/Assets/Scripts/GameManager.cs
@@ -6,21 +6,56 @@ public class GameManager : MonoBehaviour
 {
     public GameObject[] poolOfCLasses;
     public GameObject[] partyOnRun = new GameObject[4];
-    
+    public float m_HealRadius = 6.0f;
+    public LayerMask m_PlayerMask;
 
     // Start is called before the first frame update
     void Start()
     {
 
-       
+        //chooseTeam();
 
     }
-    
+
+    public Vector3 takeRandomPos(int n)
+    {
+        bool flag = true;
+        float randomX = 0.0f;
+        float randomZ = 0.0f;
+
+        while (flag)
+        {
+            randomX = Random.Range(-35.0f, 35.0f);
+            randomZ = Random.Range(-10.0f, -60.0f);
+            if (n == 0)
+            {
+                flag = false;
+            }
+            else
+            {
+                Collider[] colliders = Physics.OverlapSphere(new Vector3(randomX, 2.8f, randomZ), m_HealRadius, m_PlayerMask);
+                if (colliders.Length == 0)
+                {
+                    flag = false;
+                }
+            }
+        }
+        return new Vector3(randomX, 2.8f, randomZ);
+
+
+    }
+
     public void chooseTeam()
     {
        for(int n=0; n< partyOnRun.Length; n++)
         {
+            int rand = Random.Range(0, 4);
 
+            Vector3 spawmPos = takeRandomPos(n);
+
+            Instantiate(poolOfCLasses[rand], spawmPos, Quaternion.identity);
         }
     }
+
+ 
 }
