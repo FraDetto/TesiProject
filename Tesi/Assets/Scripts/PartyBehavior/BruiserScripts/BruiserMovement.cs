80de855b1b0b14d967c568158812861a21565989
FraDetto <francesco030996@gmail.com>
2021-04-26 19:40:40 +0200

    Working on method to move a champ in a different position if there is an other champ in the position he wants to reach.

--- Tesi/Assets/Scripts/PartyBehavior/BruiserScripts/BruiserMovement.cs
+++ Tesi/Assets/Scripts/PartyBehavior/BruiserScripts/BruiserMovement.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class BruiserMovement : MonoBehaviour
 {
     private GameObject boss;
-    private Rigidbody myRB;
+    private Rigidbody rb;
     private float initialPositionZ;
+    private float dashForce = 14.0f;
+    private bool flagDash;
 
     public float valueOfZAfterMovement = 0.0f;
 
@@ -15,12 +17,19 @@ public class BruiserMovement : MonoBehaviour
     public bool chaseFlag = false;
 
     public bool directionRight = false;
+
+
+    private bool m_HitDetect_mov_front;
+
+    RaycastHit m_Hit_mov_front;
+    public LayerMask m_PlayerMask;
+
     // Start is called before the first frame update
     void Start()
     {
         boss = GameObject.FindGameObjectWithTag("Boss");
 
-        myRB = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
 
         initialPositionZ = transform.position.z;
 
@@ -36,11 +45,24 @@ public class BruiserMovement : MonoBehaviour
 
 
             //if (toBossPos.magnitude > distanceRange)
-            if ((boss.transform.position - myRB.transform.position).magnitude > distanceRange)
+            if ((boss.transform.position - rb.transform.position).magnitude > distanceRange)
             {
+                m_HitDetect_mov_front = Physics.BoxCast(transform.position, transform.localScale, transform.forward, out m_Hit_mov_front, transform.rotation, (boss.transform.position - rb.transform.position).magnitude, m_PlayerMask);
+                if (m_HitDetect_mov_front)
+                {
+                    Debug.Log("STO HITTANDO");
+                    flagDash = true; ;
+
+                }
+                //far scegliere un altro punto in cui andare perche alleato davanti
+
                 transform.LookAt(verticalAdj);
 
-                myRB.MovePosition(transform.position + transform.forward * speed * Time.deltaTime);
+                rb.MovePosition(transform.position + transform.forward * speed * Time.deltaTime);
+
+
+               
+
 
                 valueOfZAfterMovement = initialPositionZ - transform.position.z;
             }
@@ -52,8 +74,20 @@ public class BruiserMovement : MonoBehaviour
 
     }
 
-    public void controllo()
+    private void OnDrawGizmos()
     {
-        
+        Gizmos.color = Color.red;
+        if (m_HitDetect_mov_front)
+        {
+            Gizmos.DrawRay(transform.position, transform.forward * m_Hit_mov_front.distance);
+            Gizmos.DrawWireCube(transform.position + transform.forward * m_Hit_mov_front.distance, transform.localScale);
+        }
+        else
+        {
+            //Draw a Ray forward from GameObject toward the maximum distance
+            Gizmos.DrawRay(transform.position, transform.forward * 50.0f);
+            //Draw a cube at the maximum distance
+            Gizmos.DrawWireCube(transform.position + transform.forward * 50.0f, transform.localScale);
+        }
     }
 }
