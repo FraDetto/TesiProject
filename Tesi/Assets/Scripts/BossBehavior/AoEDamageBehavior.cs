8e0de5968d783228da368bea6d600ffd0aad2147
FraDetto <francesco030996@gmail.com>
2021-05-03 20:13:05 +0200

    Added script to manage AoE attack damage on hit players. Damage management (= 0) has also been added if a player defends himself from AoE (handled differently as damage is not done as soon as it is hit)

---
+++ Tesi/Assets/Scripts/BossBehavior/AoEDamageBehavior.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AoEDamageBehavior : MonoBehaviour
+{
+    public GameObject[] playersHit = new GameObject[4];
+    public int cont = 0;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        bool flag = false;
+
+        if (null!=other.GetComponent<moreSpecificProfile>())
+        {
+            for (int i = 0; i < playersHit.Length; i++)
+            {
+                if (null!=playersHit && other.gameObject.GetInstanceID() == playersHit[i].GetInstanceID())
+                {
+                    flag = true;
+
+                }
+            }
+
+            if (!flag)
+            {
+                playersHit[cont] = other.gameObject;
+
+                //other.transform.gameObject.GetComponent<moreSpecificProfile>().publicSetLifeAfterDamage( ((damageCharacter/100)*50) );
+                cont++;
+            }
+        }
+
+       
+    }
+
+    void OnDestroy()
+    {
+        float damageCharacter = gameObject.GetComponentInParent<moreSpecificProfile>().publicGetDamageValue();
+        ///danno
+        for (int i = 0; i < cont; i++)
+        {
+            if (!playersHit[i].GetComponent<moreSpecificProfile>().publicGetIsDefending())
+            {
+                playersHit[i].GetComponent<moreSpecificProfile>().publicSetLifeAfterDamage(((damageCharacter / 100) * 50));
+            }
+             
+       
+        }
+    }
+}
