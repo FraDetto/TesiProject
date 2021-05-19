17f4765699259f43dc2a4e80746ec9f77f664691
FraDetto <francesco030996@gmail.com>
2021-05-19 11:51:09 +0200

    Added borders to prevent a champ from leaving the map

---
+++ Tesi/Assets/BorderCollisionScript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BorderCollisionScript : MonoBehaviour
+{
+    public GameObject boss;
+    public float m_HealRadius = 6.0f;
+    public LayerMask m_PlayerMask;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (null != other.GetComponent<moreSpecificProfile>())
+        {
+            other.transform.GetComponent<Rigidbody>().position = takeRandomPos();
+        }
+    }
+
+    public Vector3 takeRandomPos()
+    {
+        bool flag = true;
+        float randomX = 0.0f;
+        float randomZ = 0.0f;
+
+        while (flag)
+        {
+            randomX = Random.Range(-35.0f, 35.0f);
+            randomZ = Random.Range(-10.0f, -60.0f);
+
+            Collider[] colliders = Physics.OverlapSphere(new Vector3(randomX, 2.8f, randomZ), m_HealRadius, m_PlayerMask);
+            if (colliders.Length == 0)
+            {
+                flag = false;
+            }
+
+        }
+        return new Vector3(randomX, 2.8f, randomZ);
+
+
+    }
+}
